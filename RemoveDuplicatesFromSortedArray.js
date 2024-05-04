6328ab60dd0e137da1a8c90bfbbe8558b9bae3de
Usman Malik <usmanmalikk2004@gmail.com>
2024-05-04 14:31:33 +0500

    remove dup

--- RemoveDuplicatesFromSortedArray.js
+++ RemoveDuplicatesFromSortedArray.js
@@ -6,7 +6,7 @@ var removeDuplicates = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < i; j++) {
             if (nums[i] === nums[j]) {
-                isDuplicate = true;
+                
                 nums.splice(i,1);
                 removeDuplicates(nums);
         }
