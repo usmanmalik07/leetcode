163df1f00900e9198106af9a6477bf26640d1c2f
Usman Malik <usmanmalikk2004@gmail.com>
2024-06-05 15:25:10 +0500

    sqrt function fixed

--- sqrt.js
+++ sqrt.js
@@ -10,11 +10,9 @@ var mySqrt = function(x) {
      let left = 1;
      let right = Math.floor(x / 2);
      let result = 0;
- 
      while (left <= right) {
          const mid = Math.floor((left + right) / 2);
          const midSquared = mid * mid;
- 
          if (midSquared === x) {
              return mid;
          } else if (midSquared < x) {
