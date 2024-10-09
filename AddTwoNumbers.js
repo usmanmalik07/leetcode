cb140ddddbe9c7ffe6c9f152db6caf573140e39d
Usman Malik <usmanmalikk2004@gmail.com>
2024-10-09 23:17:03 +0500

    clean code

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -1,12 +1,4 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-
-/**
+/*
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
